1e852ea6fd6137daef1b805372f1a7381c9af256
Hagata Eduarda <hagataeduarda15@outlook.com>
2017-12-05 18:49:55 -0200

    Música Fail

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -27,4 +27,8 @@ public class AudioManager : MonoBehaviour {
 	{
 		AudioSource.PlayClipAtPoint(audioPlayer.jump, obj.transform.position);
 	}
+		public void PlayFailSound(GameObject obj)
+	{
+		AudioSource.PlayClipAtPoint(audioSFX.fail, obj.transform.position);
+	}
 }
